a68168b0846352190e0c82ae85192ef8d7f1ad13
obijuan <102243434+JuanCFalabrino@users.noreply.github.com>
2023-05-18 07:46:45 -0300

    1805

--- NetBeansProjects/JOPTION1705/JavaApplication16/src/javaapplication16/JavaApplication16.java
+++ NetBeansProjects/JOPTION1705/JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -36,9 +36,16 @@ public class JavaApplication16 {
     JOptionPane.showMessageDialog(null,"MUY BIEN", "Posistivo",JOptionPane.INFORMATION_MESSAGE,BIEN);
     
     
+    //Ejemplo de tabular en joptionpane
+    ImageIcon ALU = new ImageIcon("src/Imagenes/alumno.png");
+    String nombre2 = JOptionPane.showInputDialog(null,"Ingrese su nombre ","Nombre");
+    String apellido = JOptionPane.showInputDialog(null,"Ingrese su Apellido ","Apellido");
+    String telefono = JOptionPane.showInputDialog(null,"Ingrese su nombre ","TELEFONO");
+    
+    JOptionPane.showMessageDialog(null,"\nDATOS DEL USUARIO\n" + "Nombre: " + nombre2 + "\nApellido: " + apellido + "\nTelefono: " + telefono,"Datos del Alumno",JOptionPane.INFORMATION_MESSAGE, ALU);
+    }   
     
     
     
-    }
     
 }
